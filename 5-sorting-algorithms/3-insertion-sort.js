4a2888d87b19fb6f84c98b158520382aac923d57
Nick Papadakis <nickfin2014@gmail.com>
2021-04-27 15:27:21 -0700

    added insertion sort!

---
+++ 5-sorting-algorithms/3-insertion-sort.js
@@ -0,0 +1,37 @@
+Insertion Sort
+// Insertion sort is a simple sorting algorithm that works similar to the way you sort playing cards in your hands. The array is virtually split into a sorted and an unsorted part. Values from the unsorted part are picked and placed at the correct position in the sorted part.
+// Algorithm
+// To sort an array of size n in ascending order:
+// 1: Iterate from arr[1] to arr[n] over the array.
+// 2: Compare the current element (key) to its predecessor.
+// 3: If the key element is smaller than its predecessor, compare it to the elements before. Move the greater elements one position up to make space for the swapped element
+
+Example
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) =>
+    ([arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]);
+
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
+  }
+
+  return arr;
+}
+
+selectionSort([0,2,34,22,10,19,17]);
+
+Breakdown
+// Time Complexity: O(n^2)
+// Auxiliary Space: O(1)
+// Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+// Algorithmic Paradigm: Incremental Approach
+// Sorting In Place: Yes
+// Stable: Yes
+// Online: Yes
+// Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
