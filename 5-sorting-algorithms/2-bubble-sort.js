2e284317b5eb6b5a97afb347086453914077140a
Nick Papadakis <nickfin2014@gmail.com>
2021-04-27 15:23:21 -0700

    added bubble sort again after files magically disappeared from local machine

---
+++ 5-sorting-algorithms/2-bubble-sort.js
@@ -0,0 +1,33 @@
+Bubble Sort
+
+// //Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are in wrong order
+
+Example
+  const swap = (arr, i1, i2) => {
+    [arr[i1], arr[i2]] = [arr[i2], arr[i1]];
+  };
+
+  const bubbleSort = (arr) => {
+    let swapped = false;
+    do {
+      swapped = false;
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > arr[i + 1]) {
+          swap(arr, i, i + 1);
+          swapped = true;
+        }
+      }
+    } while (swapped)
+    return arr;
+  };
+
+  console.log(bubbleSort([1,3,2,4,0]));
+
+  More Details
+  // Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+  // Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+  // Auxiliary Space: O(1)
+
+  // Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
---
+++ 5-sorting-algorithms/6-merge-sort.js
@@ -0,0 +1,52 @@
+Merge sort
+//Merge sort is the first of the three intermediate algorithms we will consider. These algorithms are almost almost more efficient than the quadratic algorithms we covered up to this point, but are more complicated! You got this.
+
+//Merge sort utilizes the divide and conquer pattern to increase efficiency. This algorithm recursively breaks our dataset down into halves until each element is its own array, and then merges these sub-arrays! Thus, the dataset is decomposed into single element arrays and then we concatenate a bunch of merges back up the dataset until we have a new sorted whole.
+
+//That's nice and all, but let's take a look at how it works in code. You should also look at the visualizer in the 'Further Resources' section to get a firmer grasp on what is happening with merge sort.
+
+//lets look at how to merge two sorted arrays together first. This will be the building block for our mergeSort as we will need to merge sorted arrays...repeatedly.
+
+const mergeArrays = (arr1, arr2) => {
+  let result = [];
+
+  while (arr1.length && arr2.length) {
+    result.push(arr1[0] < arr2[0] ? arr1.shift() : arr2.shift());
+  }
+
+  while (arr1.length) {
+    result.push(arr1.shift());
+  }
+
+  while(arr2.length) {
+    result.push(arr2.shift());
+  }
+
+  return result;
+};
+
+//we can be fancy with ES6 and replace lines 17-25 with the following:
+//return [...arr, ...arr1, ...arr2];
+//I will leave the above implementation to make explicit what is happening, however: any remaining elements in array1 and array2 will be pushed into the result array before returning!
+
+//While both arrays have length, we compare the first value of each and see which is smaller. The smaller element is pushed to a result array (the ternary operator saves some space instead of having multiple 'if' blocks, but we could do that too. We take advantage of the 'shift' method because it simultaneously reduces the size of the arrays we are operating on AND returns  the first value of a given array, which is what we want to push to our result array).
+
+//If we still have remaining values in either of the arrays, we clean up and push them into the result array as well. Due to the two arrays being sorted already, this does not generate unsorted results (it is best to look at a visualizer or walking through with chrome  dev tools to literally SEE this).
+
+//Once we have a building block function to perform our merges, we can finish the job with a main mergeSort function body:
+
+const mergeSort1 = (arr) => {
+  if (arr.length < 2) { return arr; }
+  let mid = Math.floor(arr.length / 2);
+  let l = arr.slice(0, mid);
+  let r = arr.slice(mid);
+  let sortedL = mergeSort1(l);
+  let sortedR = mergeSort1(r);
+  return mergeArrays(sortedL, sortedR);
+};
+mergeSort1([110,34,25,26,114]);
+
+//We return an array if it is less than 2 elements because that means it is already sorted. We then divide the array in half, recursively sort each half by calling on mergeSort again and again until we have single element arrays, then merge the two halves with one final merge.
+
+Further Resources
+https://visualgo.net/en/sorting
