269bfb6f0454992afc07a7ac2939f2fc5aacc7ae
Nick Papadakis <nickfin2014@gmail.com>
2021-04-30 14:17:43 -0700

    added ES6 solution for reversing linked lists

--- 6-data-structures/3-Stacks-Queues/1-queues.js
+++ 6-data-structures/3-Stacks-Queues/1-queues.js
@@ -1,7 +1,7 @@
-// Queues
+Queues
 //Queues are often grouped with stacks, as they are rather similar in function and in complexity. A queue is much like a stack in that it is simple, can be implemented as an array, and can also have linkedlist and non-linked list implementations. Really, the sole relevant difference is that queues adhere to the FIFO model: the first item inserted is the first removed. The common metaphor is that of people in line: the first in line
 
-// Array Construction
+Array Construction
 let q1 = [];
 q1.push(1);
 q1.push(2);
@@ -11,15 +11,13 @@ q1.shift();
 
 //We add items to the list and always remove the first added, ie SHIFT the first value out of the array. However unlike stack where push and pop have ideal time complexity, shifting does not. Therefore, we really want to build our own queue out of a class. But, if performance didn't matter, the above would suffice as a queue.
 
-// Linked List Construction
-
+Linked List Construction
 class Node {
   constructor(val) {
     this.val = val;
     this.next = null;
   }
 }
-
 class Q1 {
   constructor() {
     this.first = null;
@@ -98,3 +96,6 @@ q3.queue(3);
 console.log(q3.dequeue());
 console.log(q3.dequeue());
 console.log(q3);
+
+Time complexity
+//Queues are much like stacks in that they achieve constant time with insertion and removal and linear time for search and access. Just like stacks, the time complexity for searching and access isn't particularly relevant as if you needed those to be constant, you would probably opt for a different data structure anyway. Use queues to form lines of data for such operations as browser history, or operations in FIFO order more generally. Queues (just like stacks), are also often a building block for more advanced data structures.
